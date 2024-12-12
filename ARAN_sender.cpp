d692fafd32bdc6b1a2c1cebe5b32ee8289fc0487
Ukyo <e1n21077@oit.ac.jp>
2024-12-12 14:21:32 +0000

    update

--- ARAN_sender.cpp
+++ ARAN_sender.cpp
@@ -4,6 +4,10 @@
 #include <chrono>
 #include <ctime>
 #include <iomanip>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
 
 struct RDP
@@ -37,11 +41,37 @@ int main() {
     
     //RDPを生成するコード
     RDP test_rdp1 = {"RDP","10.0.0.1","certification",rnd(),formattedTime}; //テスト用のため適当に設定
-    std::string IP = "10.0.0.1"; //sta1のIPアドレス
-    std::cout << "IP=" << IP << std::endl;
+    
+    //ブロードキャストするコード
+
+    //ソケットの作成
+    int sock;
+    struct sockaddr_in addr;
+    int yes = 1;
+
+    sock = socket(AF_INET,SOCK_DGRAM,0);
+    if (sock < 0) {
+        std::cerr << "Socket creation failed" << std::endl;
+        return 1;
+    }
+    //ブロードキャストの設定
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(12345);
+    addr.sin_addr.s_addr = inet_addr("10.255.255.255");
+
+    //ソケットオプションの設定
+    if(setsockopt(sock,SOL_SOCKET,SO_BROADCAST,reinterpret_cast<const char*>(&yes),sizeof(yes)) < 0) {
+        std::cerr << "Failed to set socket options" << std::endl;
+        close(sock);
+        return 1;
+    }
+
+    if (sendto(sock, "HELLO", 5, 0, reinterpret_cast<struct sockaddr*>(&addr), sizeof(addr)) < 0) {
+        std::cerr << "Failed to send message" << std::endl;
+        close(sock);
+        return 1;
+    }
+
+    close(sock);
 
 }
-
-
-
-//ブロードキャストするコード
