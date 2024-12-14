04a5e6c8f5b17f499216b149c9e623c5ad8458de
ukyo-hasegawa <hase.ukyo@icloud.com>
2024-12-14 09:30:19 +0000

    update

--- RSA/RSA.h
+++ RSA/RSA.h
@@ -1,6 +1,8 @@
 #include <openssl/evp.h>
+#include <openssl/pem.h>
 
 int main() {
     EVP_PKEY_CTX *KeyCtx = EVP_PKEY_CTX_new_id(EVP_PKEY_RSA,NULL);
-
+    EVP_PKEY_keygen_init(KeyCtx);
+    EVP_PKEY_CTX_set_rsa_keygen_bits(KeyCtx,4096);
 }
